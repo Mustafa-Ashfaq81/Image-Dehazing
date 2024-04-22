a598245604b6daa29c1cfd3a3b68051c7ca49893
Mustafa-Ashfaq81 <mustafaaplus52@gmail.com>
2024-04-22 23:02:57 +0500

    Final Deliverable 2

--- DehazeFormer/train.py
+++ DehazeFormer/train.py
@@ -8,145 +8,192 @@ from torch.cuda.amp import autocast, GradScaler
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
+from skimage.metrics import structural_similarity as ssim
 # import torch.nn.utils as torch_utils
 
 from utils import AverageMeter
 from datasets.loader import PairLoader
 from models import *
 
+from torchvision.models import vgg19
+from torch.nn.functional import mse_loss
+# from pytorch_msssim import ssim
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', default='dehazeformer-s', type=str, help='model name')
-parser.add_argument('--num_workers', default=2, type=int, help='number of workers')
-parser.add_argument('--no_autocast', action='store_false', default=True, help='disable autocast')
-parser.add_argument('--save_dir', default='./saved_models/', type=str, help='path to models saving')
-parser.add_argument('--data_dir', default='./data/', type=str, help='path to dataset')
-parser.add_argument('--log_dir', default='./logs/', type=str, help='path to logs')
-parser.add_argument('--dataset', default='RESIDE-IN', type=str, help='dataset name')
-parser.add_argument('--exp', default='indoor', type=str, help='experiment setting')
-parser.add_argument('--gpu', default='0,1,2,3', type=str, help='GPUs used for training')
+parser.add_argument('--model', default='dehazeformer-s',
+                    type=str, help='model name')
+parser.add_argument('--num_workers', default=2,
+                    type=int, help='number of workers')
+parser.add_argument('--no_autocast', action='store_false',
+                    default=True, help='disable autocast')
+parser.add_argument('--save_dir', default='./saved_models/',
+                    type=str, help='path to models saving')
+parser.add_argument('--data_dir', default='./data/',
+                    type=str, help='path to dataset')
+parser.add_argument('--log_dir', default='./logs/',
+                    type=str, help='path to logs')
+parser.add_argument('--dataset', default='RESIDE-IN',
+                    type=str, help='dataset name')
+parser.add_argument('--exp', default='indoor',
+                    type=str, help='experiment setting')
+parser.add_argument('--gpu', default='0,1,2,3', type=str,
+                    help='GPUs used for training')
 args = parser.parse_args()
 
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
 
 def train(train_loader, network, criterion, optimizer, scaler):
-	losses = AverageMeter()
+    losses = AverageMeter()
 
-	torch.cuda.empty_cache()
-	
-	network.train()
+    torch.cuda.empty_cache()
 
-	for batch in train_loader:
-		source_img = batch['source'].cuda()
-		# print(f'Hazy shape: {source_img.shape}')
-		target_img = batch['target'].cuda()
-		# print(f'GT shape: {target_img.shape}')
+    network.train()
 
-		with autocast(args.no_autocast):
-			output = network(source_img)
-			# print(f"Output shape: {output.shape}")
-			loss = criterion(output, target_img)
+    for batch in train_loader:
+        source_img = batch['source'].cuda()
+        # print(f'Hazy shape: {source_img.shape}')
+        target_img = batch['target'].cuda()
+        # print(f'GT shape: {target_img.shape}')
 
-		losses.update(loss.item())
+        with autocast(args.no_autocast):
+            output = network(source_img)
+            # print(f"Output shape: {output.shape}")
+            loss = criterion(output, target_img)
 
-		optimizer.zero_grad()
-		scaler.scale(loss).backward()
-		# torch_utils.clip_grad_norm_(network.parameters(), max_norm=1.0)  # Gradient clipping
-		scaler.step(optimizer)
-		scaler.update()
+        losses.update(loss.item())
 
-	return losses.avg
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        # torch_utils.clip_grad_norm_(network.parameters(), max_norm=1.0)  # Gradient clipping
+        scaler.step(optimizer)
+        scaler.update()
+
+    return losses.avg
 
 
 def valid(val_loader, network):
-	PSNR = AverageMeter()
+    PSNR = AverageMeter()
 
-	torch.cuda.empty_cache()
+    torch.cuda.empty_cache()
 
-	network.eval()
+    network.eval()
 
-	for batch in val_loader:
-		source_img = batch['source'].cuda()
-		target_img = batch['target'].cuda()
+    for batch in val_loader:
+        source_img = batch['source'].cuda()
+        target_img = batch['target'].cuda()
 
-		with torch.no_grad():							# torch.no_grad() may cause warning
-			output = network(source_img).clamp_(-1, 1)		
+        with torch.no_grad():							# torch.no_grad() may cause warning
+            output = network(source_img).clamp_(-1, 1)
 
-		mse_loss = F.mse_loss(output * 0.5 + 0.5, target_img * 0.5 + 0.5, reduction='none').mean((1, 2, 3))
-		psnr = 10 * torch.log10(1 / mse_loss).mean()
-		PSNR.update(psnr.item(), source_img.size(0))
+        mse_loss = F.mse_loss(
+            output * 0.5 + 0.5, target_img * 0.5 + 0.5, reduction='none').mean((1, 2, 3))
+        psnr = 10 * torch.log10(1 / mse_loss).mean()
+        PSNR.update(psnr.item(), source_img.size(0))
 
-	return PSNR.avg
+    return PSNR.avg
 
+class TVRegularizationLoss(nn.Module):
+    def forward(self, x):
+        # Calculate TV regularization loss
+        batch_size, channels, height, width = x.size()
+        tv_loss = torch.sum(torch.abs(x[:, :, :, :-1] - x[:, :, :, 1:])) + \
+                  torch.sum(torch.abs(x[:, :, :-1, :] - x[:, :, 1:, :]))
+        return tv_loss / (batch_size * channels * height * width)  # Normalize by image size
+
+def combined_loss(x, y):
+    mse_loss = F.mse_loss(x, y)
+    
+    # Apply TV regularization to the reconstructed image
+    tv_loss = TVRegularizationLoss()(x)
+    
+    # Convert torch tensors to NumPy arrays
+    x_np = x.detach().cpu().numpy()
+    y_np = y.detach().cpu().numpy()
+    
+    # Calculate SSIM loss for perceptual similarity
+    height, width = x_np.shape[-2:]
+    win_size = min(height, width)
+    print("he", win_size)
+    ssim_loss = 1 - ssim(x_np, y_np, data_range=1, multichannel=True, win_size=win_size)
+    
+    # Weight for TV regularization term (you can adjust this)
+    tv_weight = 0.001
+    
+    # Combine losses with appropriate weights
+    total_loss = mse_loss + tv_weight * tv_loss + ssim_loss
+    return total_loss
 
 if __name__ == '__main__':
-	setting_filename = os.path.join('configs', args.exp, args.model+'.json')
-	if not os.path.exists(setting_filename):
-		setting_filename = os.path.join('configs', args.exp, 'default.json')
-	with open(setting_filename, 'r') as f:
-		setting = json.load(f)
+    setting_filename = os.path.join('configs', args.exp, args.model+'.json')
+    if not os.path.exists(setting_filename):
+        setting_filename = os.path.join('configs', args.exp, 'default.json')
+    with open(setting_filename, 'r') as f:
+        setting = json.load(f)
 
-	network = eval(args.model.replace('-', '_'))()
-	network = nn.DataParallel(network).cuda()
+    network = eval(args.model.replace('-', '_'))()
+    network = nn.DataParallel(network).cuda()
 
-	criterion = nn.L1Loss()
+    criterion = mse_loss #combined_loss  #nn.L1Loss()
 
-	if setting['optimizer'] == 'adam':
-		optimizer = torch.optim.Adam(network.parameters(), lr=setting['lr'])
-	elif setting['optimizer'] == 'adamw':
-		optimizer = torch.optim.AdamW(network.parameters(), lr=setting['lr'])
-	else:
-		raise Exception("ERROR: unsupported optimizer") 
+    if setting['optimizer'] == 'adam':
+        optimizer = torch.optim.Adam(network.parameters(), lr=setting['lr'])
+    elif setting['optimizer'] == 'adamw':
+        optimizer = torch.optim.AdamW(network.parameters(), lr=setting['lr'])
+    else:
+        raise Exception("ERROR: unsupported optimizer")
 
-	scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=setting['epochs'], eta_min=setting['lr'] * 1e-2)
-	scaler = GradScaler()
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=setting['epochs'], eta_min=setting['lr'] * 1e-2)
+    scaler = GradScaler()
 
-	dataset_dir = os.path.join(args.data_dir, args.dataset)
-	train_dataset = PairLoader(dataset_dir, 'train', 'train', 
-								setting['patch_size'], setting['edge_decay'], setting['only_h_flip'])
-	train_loader = DataLoader(train_dataset,
+    dataset_dir = os.path.join(args.data_dir, args.dataset)
+    train_dataset = PairLoader(dataset_dir, 'train', 'train',
+                               setting['patch_size'], setting['edge_decay'], setting['only_h_flip'])
+    train_loader = DataLoader(train_dataset,
                               batch_size=setting['batch_size'],
                               shuffle=True,
                               num_workers=args.num_workers,
                               pin_memory=True,
                               drop_last=True)
-	val_dataset = PairLoader(dataset_dir, 'test', setting['valid_mode'], 
-							  setting['patch_size'])
-	val_loader = DataLoader(val_dataset,
+    val_dataset = PairLoader(dataset_dir, 'test', setting['valid_mode'],
+                             setting['patch_size'])
+    val_loader = DataLoader(val_dataset,
                             batch_size=setting['batch_size'],
                             num_workers=args.num_workers,
                             pin_memory=True)
 
-	save_dir = os.path.join(args.save_dir, args.exp)
-	os.makedirs(save_dir, exist_ok=True)
+    save_dir = os.path.join(args.save_dir, args.exp)
+    os.makedirs(save_dir, exist_ok=True)
 
-	if not os.path.exists(os.path.join(save_dir, args.model+'.pth')):
-		print('==> Start training, current model name: ' + args.model)
-		# print(network)
+    if not os.path.exists(os.path.join(save_dir, args.model+'.pth')):
+        print('==> Start training, current model name: ' + args.model)
+        # print(network)
 
-		writer = SummaryWriter(log_dir=os.path.join(args.log_dir, args.exp, args.model))
+        writer = SummaryWriter(log_dir=os.path.join(
+            args.log_dir, args.exp, args.model))
 
-		best_psnr = 0
-		for epoch in tqdm(range(setting['epochs'] + 1)):
-			loss = train(train_loader, network, criterion, optimizer, scaler)
+        best_psnr = 0
+        for epoch in tqdm(range(setting['epochs'] + 1)):
+            loss = train(train_loader, network, criterion, optimizer, scaler)
 
-			writer.add_scalar('train_loss', loss, epoch)
+            writer.add_scalar('train_loss', loss, epoch)
 
-			scheduler.step()
+            scheduler.step()
 
-			if epoch % setting['eval_freq'] == 0:
-				avg_psnr = valid(val_loader, network)
-				
-				writer.add_scalar('valid_psnr', avg_psnr, epoch)
+            if epoch % setting['eval_freq'] == 0:
+                avg_psnr = valid(val_loader, network)
 
-				if avg_psnr > best_psnr:
-					best_psnr = avg_psnr
-					torch.save({'state_dict': network.state_dict()},
-                			   os.path.join(save_dir, args.model+'.pth'))
-				
-				writer.add_scalar('best_psnr', best_psnr, epoch)
+                writer.add_scalar('valid_psnr', avg_psnr, epoch)
 
-	else:
-		print('==> Existing trained model')
-		exit(1)
+                if avg_psnr > best_psnr:
+                    best_psnr = avg_psnr
+                    torch.save({'state_dict': network.state_dict()},
+                               os.path.join(save_dir, args.model+'.pth'))
+
+                writer.add_scalar('best_psnr', best_psnr, epoch)
+
+    else:
+        print('==> Existing trained model')
+        exit(1)
